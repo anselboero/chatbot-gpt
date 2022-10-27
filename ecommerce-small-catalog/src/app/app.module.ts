48172e10221a2248bc27d7adf16bb53d038d1cf7
P-aolo <occhetto.paolo@gmail.com>
2022-10-27 23:20:58 +0200

    A.C. Manza

--- ecommerce-small-catalog/src/app/app.module.ts
+++ ecommerce-small-catalog/src/app/app.module.ts
@@ -1,21 +1,20 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { FormsModule } from '@angular/forms';
+
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { HeroesComponent } from './heroes/heroes.component';
-import { TestComponent } from './test/test.component';
+import { MenuBarComponent } from './menu-bar/menu-bar.component';
+import { MenuEntryComponent } from './menu-entry/menu-entry.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HeroesComponent,
-    TestComponent
+    MenuBarComponent,
+    MenuEntryComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule,
-    FormsModule
+    AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ ecommerce-small-catalog/src/app/menu-entry/menu-entry.component.ts
@@ -0,0 +1,26 @@
+import { Component, Input, OnInit } from '@angular/core';
+@Component({
+  selector: 'app-menu-entry',
+  templateUrl: './menu-entry.component.html',
+  styleUrls: ['./menu-entry.component.css']
+})
+export class MenuEntryComponent implements OnInit {
+  @Input() 
+  entryName ?: string;
+  isSelected ?:boolean;
+  dropDownMenu : string[] = [];
+  constructor() { 
+  }
+
+  ngOnInit(): void {
+    for(let i = 0; i<4; i++) {
+      console.log('test ' + i + this.entryName);
+      if(this.entryName) this.dropDownMenu[i]=this.entryName+i;
+      console.log(this.dropDownMenu);
+    }
+  }
+  selectEntry() : void {
+    console.log('test');
+    this.isSelected = true;
+  }
+}
