f902ec27a616f1e4f8fa6b7aac8a83640134b60b
P-aolo <occhetto.paolo@gmail.com>
2022-11-07 23:06:52 +0100

    created components

--- ecommerce-small-catalog/src/app/app.module.ts
+++ ecommerce-small-catalog/src/app/app.module.ts
@@ -6,12 +6,20 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { MenuBarComponent } from './menu-bar/menu-bar.component';
 import { MenuEntryComponent } from './menu-entry/menu-entry.component';
+import { ShopPageComponent } from './shop-page/shop-page.component';
+import { AboutUsPageComponent } from './about-us-page/about-us-page.component';
+import { CartPageComponent } from './cart-page/cart-page.component';
+import { HomePageComponent } from './home-page/home-page.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     MenuBarComponent,
-    MenuEntryComponent
+    MenuEntryComponent,
+    ShopPageComponent,
+    AboutUsPageComponent,
+    CartPageComponent,
+    HomePageComponent
   ],
   imports: [
     BrowserModule,
