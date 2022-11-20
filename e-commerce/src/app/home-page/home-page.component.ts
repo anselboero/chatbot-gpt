04301da072b3d8d927c222a0162032194765cb8a
ansel <anselboero@yahoo.it>
2022-11-20 22:47:50 +0100

    added carousel to home-page

--- e-commerce/src/app/home-page/home-page.component.ts
+++ e-commerce/src/app/home-page/home-page.component.ts
@@ -5,11 +5,29 @@ import { Component, OnInit, HostBinding } from '@angular/core';
   templateUrl: './home-page.component.html',
   styleUrls: ['./home-page.component.css']
 })
-export class HomePageComponent implements OnInit {
-  @HostBinding('class') cssClass = 'bg-img1'; 
-  constructor() { }
+export class HomePageComponent implements OnInit { 
+  imagesRootPath = "../../assets/background/";
+  images = [
+    this.imagesRootPath + 'img_1.jpg',
+    this.imagesRootPath + 'img_2.jpg',
+    this.imagesRootPath + 'img_3.jpg',
+    this.imagesRootPath + 'img_4.jpg',
+    this.imagesRootPath + 'video_1.gif'
+  ];
+  currentImage!: string;
+  changeBackgroundCounter = 0;
+
+  
+  constructor() {
+   }
 
   ngOnInit(): void {
+    this.currentImage = this.images[0];
+    setInterval(() => {
+      this.changeBackgroundCounter=(this.changeBackgroundCounter+1)%this.images.length;
+      this.currentImage = this.images[this.changeBackgroundCounter];
+    },7000
+    );
   }
 
 }
